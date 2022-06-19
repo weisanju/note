51f0818b31d0f55b38ecce263aa4e542ccfecdc5
肖佳权 <1259103745@qq.com>
2022-06-19 18:37:32 +0800

    java,python:updateMsg

--- setEnvAndBuild.py
+++ setEnvAndBuild.py
@@ -3,8 +3,8 @@ a=os.popen('git log --pretty=format:%s -1')
 commit_msg = a.readline()
 
 update_module=commit_msg.split(":")
-with open("setEnv.sh", mode='w', encoding='utf-8') as file_obj:
+with open("setEnv.sh", mode='w') as file_obj:
     if len(update_module)>1:
         for key in update_module[0].split(","):
-            file_obj.write("{}=1\n".format(key))
+            file_obj.write("export {}=1\n".format(key))
             os.system("mdbook build src/{}".format(key))
